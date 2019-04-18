bc7e0b8b5389e542dfb76b2cac0a7ea0ffd6fc39
MarcosRDP <marcos.r.peres@hotmail.com>
2019-04-18 13:26:58 -0300

    Add files via upload

--- db/db_connect.php
+++ db/db_connect.php
@@ -2,8 +2,8 @@
 
 // Conexão com banco de dados
 $servername = "localhost";
-$username = "sudo";
-$password = "#NeoLords2";
+$username = "labileel";
+$password = "oQpEspOAYMtCcHKH";
 $dbname = "dataset";
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
